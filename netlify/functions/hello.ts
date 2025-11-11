621fc99f6351471a10a0634922b1f5c7dd5038eb
Mike Hodnick <mike@kindohm.com>
2025-11-11 11:59:25 -0600

    add typescript

--- netlify/functions/hello.ts
+++ netlify/functions/hello.ts
@@ -1,4 +1,4 @@
-export async function handler(event, context) {
+export async function handler() {
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "Hello from Netlify Functions!" }),
